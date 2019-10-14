804d4399670a2a42ab255213cb769d611c714a2c
Nikolay Stanchev <nstanchev@vmware.com>
2019-10-14 09:15:12 +0300

    Add Unmarshalling Validation to Channel
    Fix #136.
    
    Signed-off-by: Nikolay Stanchev <nstanchev@vmware.com>

--- models/channel.go
+++ models/channel.go
@@ -24,6 +24,67 @@ type Channel struct {
 	Type          ChannelType `json:"type,omitempty"`          // Type indicates whether the channel facilitates email or REST
 	MailAddresses []string    `json:"mailAddresses,omitempty"` // MailAddresses contains email addresses
 	Url           string      `json:"url,omitempty"`           // URL contains a REST API destination
+	isValidated   bool
+}
+
+// MarshalJSON implements the Marshaler interface. Empty strings will be null.
+func (c Channel) MarshalJSON() ([]byte, error) {
+	test := struct {
+		Type          ChannelType `json:"type,omitempty"`
+		MailAddresses []string    `json:"mailAddresses,omitempty"`
+		Url           string      `json:"url,omitempty"`
+	}{
+		Type:          c.Type,
+		MailAddresses: c.MailAddresses,
+	}
+
+	if c.Url != "" {
+		test.Url = c.Url
+	}
+
+	return json.Marshal(test)
+}
+
+// UnmarshalJSON implements the Unmarshaler interface for the Command type
+func (c *Channel) UnmarshalJSON(data []byte) error {
+	var err error
+	a := new(struct {
+		Type          ChannelType `json:"type,omitempty"`          // Type indicates whether the channel facilitates email or REST
+		MailAddresses []string    `json:"mailAddresses,omitempty"` // MailAddresses contains email addresses
+		Url           string      `json:"url,omitempty"`           // URL contains a REST API destination
+	})
+
+	// Error with unmarshaling
+	if err = json.Unmarshal(data, a); err != nil {
+		return err
+	}
+
+	// Check nil fields
+	if a.MailAddresses != nil {
+		c.MailAddresses = a.MailAddresses
+	}
+	c.Type = a.Type
+	c.isValidated, err = c.Validate()
+
+	return err
+}
+
+// Validate satisfies the Validator interface
+func (c Channel) Validate() (bool, error) {
+	if !c.isValidated {
+		err := validate(c)
+		if err != nil {
+			return false, err
+		}
+		if c.Type == "REST" && c.Url == "" {
+			return false, NewErrContractInvalid("ChannelType REST but no URL given")
+		}
+		if c.Type == "EMAIL" && (c.MailAddresses == nil || len(c.MailAddresses) == 0) {
+			return false, NewErrContractInvalid("ChannelType EMAIL but no MailAddresses given")
+		}
+		return true, nil
+	}
+	return c.isValidated, nil
 }
 
 func (c Channel) String() string {
--- models/channel_test.go
+++ models/channel_test.go
@@ -16,6 +16,7 @@
 package models
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -38,6 +39,97 @@ func TestChannel_String(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := tt.c.String(); got != tt.want {
 				t.Errorf("Channel.String() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestChannel_MarshalJSON(t *testing.T) {
+	emptyChannelBytes := []byte(TestEmptyChannel.String())
+	validEChannelBytes := []byte(TestEChannel.String())
+	validRChannelBytes := []byte(TestRChannel.String())
+
+	tests := []struct {
+		name    string
+		channel *Channel
+		want    []byte
+		wantErr bool
+	}{
+		{"test marshal of empty channel", &TestEmptyChannel, emptyChannelBytes, false},
+		{"test marshal of email channel", &TestEChannel, validEChannelBytes, false},
+		{"test marshal of rest channel", &TestRChannel, validRChannelBytes, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := Channel{
+				MailAddresses: tt.channel.MailAddresses,
+				Url:           tt.channel.Url,
+				Type:          tt.channel.Type,
+			}
+			got, err := c.MarshalJSON()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Channel.MarshalJSON() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Channel.MarshalJSON() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestChannel_UnmarshalJSON(t *testing.T) {
+
+	validRChannel := TestRChannel
+	validEChannel := TestEChannel
+
+	validRChannelJSON, _ := validRChannel.MarshalJSON()
+	validEChannelJSON, _ := validEChannel.MarshalJSON()
+	tests := []struct {
+		name    string
+		as      Channel
+		arg     []byte
+		wantErr bool
+	}{
+		{"test unmarshal rest channel", validRChannel, validRChannelJSON, false},
+		{"test unmarshal email channel", validEChannel, validEChannelJSON, false},
+		{"test invalid unmarshal email channel", validEChannel, []byte("\"{}\""), true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.as.UnmarshalJSON(tt.arg); (err != nil) != tt.wantErr {
+				t.Errorf("Channel.UnmarshalJSON() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestChannel_Validate(t *testing.T) {
+
+	validRChannel := TestRChannel
+	validEChannel := TestEChannel
+	invalidRChannel := TestRChannel
+	invalidRChannel.Url = ""
+	invalidEChannel := TestEChannel
+	invalidEChannel.MailAddresses = nil
+	invalidEChannelField := TestEChannel
+	invalidEChannelField.Type = ChannelType("Error")
+
+	tests := []struct {
+		name    string
+		as      Channel
+		wantErr bool
+	}{
+		{"test valid rest channel", validRChannel, false},
+		{"test valid email channel", validEChannel, false},
+		{"test invalid unmarshal email channel", invalidEChannel, true},
+		{"test invalid unmarshal rest channel", invalidRChannel, true},
+		{"test invalid unmarshal email channel's field", invalidEChannelField, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if _, err := tt.as.Validate(); (err != nil) != tt.wantErr {
+				t.Errorf("Channel.Validate() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
