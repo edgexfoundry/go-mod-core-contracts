9399be91232bd76755e7ae333e01c82a41cbe7e6
Brandon Forster <brandonforster@users.noreply.github.com>
2019-10-18 09:00:56 -0500

    #143: Added more models to serialize zero values as empty JSON objects. (#175)
    Signed-off-by: Brandon Forster <brandonforster@gmail.com>
    Signed-off-by: Daniel Harms <jdharms@gmail.com>

--- models/interval.go
+++ models/interval.go
@@ -17,6 +17,7 @@ package models
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"regexp"
 	"time"
 )
@@ -29,50 +30,40 @@ const (
 // Interval a period of time
 type Interval struct {
 	Timestamps  Timestamps
-	ID          string `json:"id"`
-	Name        string `json:"name"`      // non-database identifier for a shcedule (*must be quitue)
-	Start       string `json:"start"`     // Start time i ISO 8601 format YYYYMMDD'T'HHmmss 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyymmdd'T'HHmmss")
-	End         string `json:"end"`       // Start time i ISO 8601 format YYYYMMDD'T'HHmmss 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyymmdd'T'HHmmss")
-	Frequency   string `json:"frequency"` // how frequently should the event occur according ISO 8601
-	Cron        string `json:"cron"`      // cron styled regular expression indicating how often the action under interval should occur.  Use either runOnce, frequency or cron and not all.
-	RunOnce     bool   `json:"runOnce"`   // boolean indicating that this interval runs one time - at the time indicated by the start
+	ID          string `json:"id,omitempty"`
+	Name        string `json:"name,omitempty"`      // non-database identifier for a shcedule (*must be quitue)
+	Start       string `json:"start,omitempty"`     // Start time i ISO 8601 format YYYYMMDD'T'HHmmss 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyymmdd'T'HHmmss")
+	End         string `json:"end,omitempty"`       // Start time i ISO 8601 format YYYYMMDD'T'HHmmss 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyymmdd'T'HHmmss")
+	Frequency   string `json:"frequency,omitempty"` // how frequently should the event occur according ISO 8601
+	Cron        string `json:"cron,omitempty"`      // cron styled regular expression indicating how often the action under interval should occur.  Use either runOnce, frequency or cron and not all.
+	RunOnce     bool   `json:"runOnce,omitempty"`   // boolean indicating that this interval runs one time - at the time indicated by the start
 	isValidated bool   // internal member used for validation check
 }
 
 // Custom marshaling to make empty strings null
 func (i Interval) MarshalJSON() ([]byte, error) {
 	test := struct {
-		Timestamps Timestamps `json:",inline"`
-		ID         *string    `json:"id,omitempty"`
-		Name       *string    `json:"name,omitempty"`      // non-database identifier for a shcedule (*must be quitue)
-		Start      *string    `json:"start,omitempty"`     // Start time i ISO 8601 format YYYYMMDD'T'HHmmss 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyymmdd'T'HHmmss")
-		End        *string    `json:"end,omitempty"`       // Start time i ISO 8601 format YYYYMMDD'T'HHmmss 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyymmdd'T'HHmmss")
-		Frequency  *string    `json:"frequency,omitempty"` // how frequently should the event occur
-		Cron       *string    `json:"cron,omitempty"`      // cron styled regular expression indicating how often the action under schedule should occur.  Use either runOnce, frequency or cron and not all.
-		RunOnce    bool       `json:"runOnce,omitempty"`   // boolean indicating that this interval runs one time - at the time indicated by the start
+		Timestamps *Timestamps `json:",omitempty"`
+		ID         string      `json:"id,omitempty"`
+		Name       string      `json:"name,omitempty"`      // non-database identifier for a schedule (*must be quitue)
+		Start      string      `json:"start,omitempty"`     // Start time i ISO 8601 format YYYYMMDD'T'HHmmss 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyymmdd'T'HHmmss")
+		End        string      `json:"end,omitempty"`       // Start time i ISO 8601 format YYYYMMDD'T'HHmmss 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyymmdd'T'HHmmss")
+		Frequency  string      `json:"frequency,omitempty"` // how frequently should the event occur
+		Cron       string      `json:"cron,omitempty"`      // cron styled regular expression indicating how often the action under schedule should occur.  Use either runOnce, frequency or cron and not all.
+		RunOnce    bool        `json:"runOnce,omitempty"`   // boolean indicating that this interval runs one time - at the time indicated by the start
 	}{
-		Timestamps: i.Timestamps,
+		Timestamps: &i.Timestamps,
+		ID:         i.ID,
+		Name:       i.Name,
+		Start:      i.Start,
+		End:        i.End,
+		Frequency:  i.Frequency,
+		Cron:       i.Cron,
 		RunOnce:    i.RunOnce,
 	}
 
-	// Empty strings are null
-	if i.ID != "" {
-		test.ID = &i.ID
-	}
-	if i.Name != "" {
-		test.Name = &i.Name
-	}
-	if i.Start != "" {
-		test.Start = &i.Start
-	}
-	if i.End != "" {
-		test.End = &i.End
-	}
-	if i.Frequency != "" {
-		test.Frequency = &i.Frequency
-	}
-	if i.Cron != "" {
-		test.Cron = &i.Cron
+	if reflect.DeepEqual(i.Timestamps, Timestamps{}) {
+		test.Timestamps = nil
 	}
 
 	return json.Marshal(test)
@@ -82,14 +73,14 @@ func (i Interval) MarshalJSON() ([]byte, error) {
 func (i *Interval) UnmarshalJSON(data []byte) error {
 	var err error
 	type Alias struct {
-		Timestamps Timestamps `json:",inline"`
-		ID         *string    `json:"id"`
-		Name       *string    `json:"name"`
-		Start      *string    `json:"start"`
-		End        *string    `json:"end"`
-		Frequency  *string    `json:"frequency"`
-		Cron       *string    `json:"cron"`
-		RunOnce    bool       `json:"runOnce"`
+		Timestamps Timestamps `json:",omitempty"`
+		ID         *string    `json:"id,omitempty"`
+		Name       *string    `json:"name,omitempty"`
+		Start      *string    `json:"start,omitempty"`
+		End        *string    `json:"end,omitempty"`
+		Frequency  *string    `json:"frequency,omitempty"`
+		Cron       *string    `json:"cron,omitempty"`
+		RunOnce    bool       `json:"runOnce,omitempty"`
 	}
 	a := Alias{}
 	// Error with unmarshaling
@@ -167,11 +158,9 @@ func (i Interval) Validate() (bool, error) {
 	return i.isValidated, nil
 }
 
-/*
- * To String function for Interval
- */
-func (dp Interval) String() string {
-	out, err := json.Marshal(dp)
+// String returns a JSON encoded string representation of this Interval
+func (i Interval) String() string {
+	out, err := json.Marshal(i)
 	if err != nil {
 		return err.Error()
 	}
