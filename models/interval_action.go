24533fbc766978765ebdfe1b503c8c15fcbe40d2
Brandon Forster <brandonforster@users.noreply.github.com>
2019-10-18 09:00:56 -0500

    #143: Added more models to serialize zero values as empty JSON objects. (#175)
    Signed-off-by: Brandon Forster <brandonforster@gmail.com>

--- models/interval_action.go
+++ models/interval_action.go
@@ -21,94 +21,24 @@ import (
 )
 
 type IntervalAction struct {
-	ID          string `json:"id"`
-	Created     int64  `json:"created"`
-	Modified    int64  `json:"modified"`
-	Origin      int64  `json:"origin"`
-	Name        string `json:"name"`
-	Interval    string `json:"interval"`
-	Parameters  string `json:"parameters"`
-	Target      string `json:"target"`
-	Protocol    string `json:"protocol"`
-	HTTPMethod  string `json:"httpMethod"`
-	Address     string `json:"address"`
-	Port        int    `json:"port"`
-	Path        string `json:"path"`
-	Publisher   string `json:"publisher"`
-	User        string `json:"user"`
-	Password    string `json:"password"`
-	Topic       string `json:"topic"`
+	ID          string `json:"id,omitempty"`
+	Created     int64  `json:"created,omitempty"`
+	Modified    int64  `json:"modified,omitempty"`
+	Origin      int64  `json:"origin,omitempty"`
+	Name        string `json:"name,omitempty"`
+	Interval    string `json:"interval,omitempty"`
+	Parameters  string `json:"parameters,omitempty"`
+	Target      string `json:"target,omitempty"`
+	Protocol    string `json:"protocol,omitempty"`
+	HTTPMethod  string `json:"httpMethod,omitempty"`
+	Address     string `json:"address,omitempty"`
+	Port        int    `json:"port,omitempty"`
+	Path        string `json:"path,omitempty"`
+	Publisher   string `json:"publisher,omitempty"`
+	User        string `json:"user,omitempty"`
+	Password    string `json:"password,omitempty"`
+	Topic       string `json:"topic,omitempty"`
 	isValidated bool   // internal member used for validation check
-}
-
-func (ia IntervalAction) MarshalJSON() ([]byte, error) {
-	test := struct {
-		ID         *string `json:"id,omitempty"`
-		Created    int64   `json:"created,omitempty"`
-		Modified   int64   `json:"modified,omitempty"`
-		Origin     int64   `json:"origin,omitempty"`
-		Name       *string `json:"name,omitempty"`
-		Interval   *string `json:"interval,omitempty"`
-		Parameters *string `json:"parameters,omitempty"`
-		Target     *string `json:"target,omitempty"`
-		Protocol   *string `json:"protocol,omitempty"`
-		HTTPMethod *string `json:"httpMethod,omitempty"`
-		Address    *string `json:"address,omitempty"`
-		Port       int     `json:"port,omitempty"`
-		Path       *string `json:"path,omitempty"`
-		Publisher  *string `json:"publisher,omitempty"`
-		User       *string `json:"user,omitempty"`
-		Password   *string `json:"password,omitempty"`
-		Topic      *string `json:"topic,omitempty"`
-	}{
-		Created:  ia.Created,
-		Modified: ia.Modified,
-		Origin:   ia.Origin,
-		Port:     ia.Port,
-	}
-
-	// Empty strings are null
-	if ia.ID != "" {
-		test.ID = &ia.ID
-	}
-	if ia.Name != "" {
-		test.Name = &ia.Name
-	}
-	if ia.Interval != "" {
-		test.Interval = &ia.Interval
-	}
-	if ia.Parameters != "" {
-		test.Parameters = &ia.Parameters
-	}
-	if ia.Target != "" {
-		test.Target = &ia.Target
-	}
-	if ia.Protocol != "" {
-		test.Protocol = &ia.Protocol
-	}
-	if ia.HTTPMethod != "" {
-		test.HTTPMethod = &ia.HTTPMethod
-	}
-	if ia.Address != "" {
-		test.Address = &ia.Address
-	}
-	if ia.Path != "" {
-		test.Path = &ia.Path
-	}
-	if ia.Publisher != "" {
-		test.Publisher = &ia.Publisher
-	}
-	if ia.User != "" {
-		test.User = &ia.User
-	}
-	if ia.Password != "" {
-		test.Password = &ia.Password
-	}
-	if ia.Topic != "" {
-		test.Topic = &ia.Topic
-	}
-
-	return json.Marshal(test)
 }
 
 // UnmarshalJSON implements the Unmarshaler interface for the IntervalAction type
