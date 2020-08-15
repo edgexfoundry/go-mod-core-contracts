7a8de458d9e09894234e48f3226296097fbbe887
weichou <weichou1229@gmail.com>
2020-08-15 11:17:07 +0800

    feat(metadata): Add Versionable to V2 Response DTO
    Add apiVersion to device and deviceProfile, deviceService DTO
    
    Fix #266
    
    Signed-off-by: weichou <weichou1229@gmail.com>

--- v2/dtos/device.go
+++ v2/dtos/device.go
@@ -5,25 +5,28 @@
 
 package dtos
 
+import "github.com/edgexfoundry/go-mod-core-contracts/v2/dtos/common"
+
 // Device represents a registered device participating within the EdgeX Foundry ecosystem
 // This object and its properties correspond to the Device object in the APIv2 specification:
 // https://app.swaggerhub.com/apis-docs/EdgeXFoundry1/core-metadata/2.x#/Device
 type Device struct {
-	Id             string                        `json:"id,omitempty"`
-	Created        int64                         `json:"created,omitempty"`
-	Modified       int64                         `json:"modified,omitempty"`
-	Name           string                        `json:"name" validate:"required"`
-	Description    string                        `json:"description,omitempty"`
-	AdminState     string                        `json:"adminState" validate:"oneof='LOCKED' 'UNLOCKED'"`
-	OperatingState string                        `json:"operatingState" validate:"oneof='ENABLED' 'DISABLED'"`
-	LastConnected  int64                         `json:"lastConnected,omitempty"`
-	LastReported   int64                         `json:"lastReported,omitempty"`
-	Labels         []string                      `json:"labels,omitempty"`
-	Location       interface{}                   `json:"location,omitempty"`
-	ServiceName    string                        `json:"serviceName" validate:"required"`
-	ProfileName    string                        `json:"profileName" validate:"required"`
-	AutoEvents     []AutoEvent                   `json:"autoEvents,omitempty" validate:"dive"`
-	Protocols      map[string]ProtocolProperties `json:"protocols,omitempty" validate:"required,gt=0"`
+	common.Versionable `json:",inline"`
+	Id                 string                        `json:"id,omitempty"`
+	Created            int64                         `json:"created,omitempty"`
+	Modified           int64                         `json:"modified,omitempty"`
+	Name               string                        `json:"name" validate:"required"`
+	Description        string                        `json:"description,omitempty"`
+	AdminState         string                        `json:"adminState" validate:"oneof='LOCKED' 'UNLOCKED'"`
+	OperatingState     string                        `json:"operatingState" validate:"oneof='ENABLED' 'DISABLED'"`
+	LastConnected      int64                         `json:"lastConnected,omitempty"`
+	LastReported       int64                         `json:"lastReported,omitempty"`
+	Labels             []string                      `json:"labels,omitempty"`
+	Location           interface{}                   `json:"location,omitempty"`
+	ServiceName        string                        `json:"serviceName" validate:"required"`
+	ProfileName        string                        `json:"profileName" validate:"required"`
+	AutoEvents         []AutoEvent                   `json:"autoEvents,omitempty" validate:"dive"`
+	Protocols          map[string]ProtocolProperties `json:"protocols,omitempty" validate:"required,gt=0"`
 }
 
 // UpdateDevice represents a registered device participating within the EdgeX Foundry ecosystem
--- v2/dtos/deviceprofile.go
+++ v2/dtos/deviceprofile.go
@@ -5,20 +5,23 @@
 
 package dtos
 
+import "github.com/edgexfoundry/go-mod-core-contracts/v2/dtos/common"
+
 // DeviceProfile represents the attributes and operational capabilities of a device. It is a template for which
 // there can be multiple matching devices within a given system.
 // This object and its properties correspond to the DeviceProfile object in the APIv2 specification:
 // https://app.swaggerhub.com/apis-docs/EdgeXFoundry1/core-metadata/2.x#/DeviceProfile
 type DeviceProfile struct {
-	Id              string            `json:"id,omitempty"`
-	Name            string            `json:"name" yaml:"name" validate:"required" `
-	Manufacturer    string            `json:"manufacturer,omitempty" yaml:"manufacturer,omitempty"`
-	Description     string            `json:"description,omitempty" yaml:"description,omitempty"`
-	Model           string            `json:"model,omitempty" yaml:"model,omitempty"`
-	Labels          []string          `json:"labels,omitempty" yaml:"labels,flow,omitempty"`
-	DeviceResources []DeviceResource  `json:"deviceResources" yaml:"deviceResources" validate:"required,gt=0,dive"`
-	DeviceCommands  []ProfileResource `json:"deviceCommands,omitempty" yaml:"deviceCommands,omitempty" validate:"dive"`
-	CoreCommands    []Command         `json:"coreCommands,omitempty" yaml:"coreCommands,omitempty" validate:"dive"`
+	common.Versionable `json:",inline"`
+	Id                 string            `json:"id,omitempty"`
+	Name               string            `json:"name" yaml:"name" validate:"required" `
+	Manufacturer       string            `json:"manufacturer,omitempty" yaml:"manufacturer,omitempty"`
+	Description        string            `json:"description,omitempty" yaml:"description,omitempty"`
+	Model              string            `json:"model,omitempty" yaml:"model,omitempty"`
+	Labels             []string          `json:"labels,omitempty" yaml:"labels,flow,omitempty"`
+	DeviceResources    []DeviceResource  `json:"deviceResources" yaml:"deviceResources" validate:"required,gt=0,dive"`
+	DeviceCommands     []ProfileResource `json:"deviceCommands,omitempty" yaml:"deviceCommands,omitempty" validate:"dive"`
+	CoreCommands       []Command         `json:"coreCommands,omitempty" yaml:"coreCommands,omitempty" validate:"dive"`
 }
 
 // UpdateDeviceProfile represents the attributes and operational capabilities of a device. It is a template for which
