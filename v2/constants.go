da904d60a389a71d9a4b30a098d5be77849155bc
weichou <weichou1229@gmail.com>
2020-09-29 13:45:22 +0800

    feat(metadata): Add func to transform the deviceProfile model to DTO
    Add functions to transform device profile, deviceResources, deviceCommands, coreCommands model to DTO,
    
    Fix #310
    
    Signed-off-by: weichou <weichou1229@gmail.com>

--- v2/constants.go
+++ v2/constants.go
@@ -7,33 +7,35 @@ package v2
 
 // Constants related to defined routes in the v2 service APIs
 const (
-	ApiVersion                 = "v2"
-	ApiBase                    = "/api/v2"
-	ApiEventRoute              = ApiBase + "/event"
-	ApiAllEventRoute           = ApiEventRoute + "/" + All
-	ApiEventIdRoute            = ApiEventRoute + "/" + Id + "/{" + Id + "}"
-	ApiEventPushRoute          = ApiEventRoute + "/" + Pushed
-	ApiEventCountRoute         = ApiEventRoute + "/" + Count
-	ApiEventCountByDeviceRoute = ApiEventCountRoute + "/" + Device + "/{" + DeviceIdParam + "}"
-	ApiEventByDeviceRoute      = ApiEventRoute + "/" + Device + "/{" + DeviceIdParam + "}"
-	ApiAllEventByDeviceRoute   = ApiEventByDeviceRoute + "/" + All
-	ApiEventByTimeRangeRoute   = ApiEventRoute + "/" + Start + "/{" + Start + "}/" + End + "/{" + End + "}"
-	ApiEventByAgeRoute         = ApiEventRoute + "/" + Age + "/{" + Age + "}"
-	ApiEventScrubRoute         = ApiEventRoute + "/" + Scrub
-	ApiReadingRoute            = ApiBase + "/reading"
-	ApiAllReadingRoute         = ApiReadingRoute + "/" + All
-	ApiReadingCountRoute       = ApiReadingRoute + "/" + Count
-	ApiReadingIdRoute          = ApiReadingRoute + "/" + Id + "/{" + Id + "}"
-	ApiAllReadingByDeviceRoute = ApiReadingRoute + "/" + Device + "/{" + DeviceIdParam + "}/" + All
-	ApiReadingByTypeRoute      = ApiReadingRoute + "/" + Type + "/{" + Type + "}"
-	ApiReadingByTimeRangeRoute = ApiReadingRoute + "/" + Start + "/{" + Start + "}/" + End + "/{" + End + "}"
-	ApiDeviceProfileRoute      = ApiBase + "/deviceprofile"
-	ApiDeviceRoute             = ApiBase + "/device"
-	ApiDeviceServiceRoute      = ApiBase + "/deviceservice"
-	ApiConfigRoute             = ApiBase + "/config"
-	ApiMetricsRoute            = ApiBase + "/metrics"
-	ApiPingRoute               = ApiBase + "/ping"
-	ApiVersionRoute            = ApiBase + "/version"
+	ApiVersion                      = "v2"
+	ApiBase                         = "/api/v2"
+	ApiEventRoute                   = ApiBase + "/event"
+	ApiAllEventRoute                = ApiEventRoute + "/" + All
+	ApiEventIdRoute                 = ApiEventRoute + "/" + Id + "/{" + Id + "}"
+	ApiEventPushRoute               = ApiEventRoute + "/" + Pushed
+	ApiEventCountRoute              = ApiEventRoute + "/" + Count
+	ApiEventCountByDeviceRoute      = ApiEventCountRoute + "/" + Device + "/{" + DeviceIdParam + "}"
+	ApiEventByDeviceRoute           = ApiEventRoute + "/" + Device + "/{" + DeviceIdParam + "}"
+	ApiAllEventByDeviceRoute        = ApiEventByDeviceRoute + "/" + All
+	ApiEventByTimeRangeRoute        = ApiEventRoute + "/" + Start + "/{" + Start + "}/" + End + "/{" + End + "}"
+	ApiEventByAgeRoute              = ApiEventRoute + "/" + Age + "/{" + Age + "}"
+	ApiEventScrubRoute              = ApiEventRoute + "/" + Scrub
+	ApiReadingRoute                 = ApiBase + "/reading"
+	ApiAllReadingRoute              = ApiReadingRoute + "/" + All
+	ApiReadingCountRoute            = ApiReadingRoute + "/" + Count
+	ApiReadingIdRoute               = ApiReadingRoute + "/" + Id + "/{" + Id + "}"
+	ApiAllReadingByDeviceRoute      = ApiReadingRoute + "/" + Device + "/{" + DeviceIdParam + "}/" + All
+	ApiReadingByTypeRoute           = ApiReadingRoute + "/" + Type + "/{" + Type + "}"
+	ApiReadingByTimeRangeRoute      = ApiReadingRoute + "/" + Start + "/{" + Start + "}/" + End + "/{" + End + "}"
+	ApiDeviceProfileRoute           = ApiBase + "/deviceprofile"
+	ApiDeviceProfileUploadFileRoute = ApiDeviceProfileRoute + "/uploadfile"
+	ApiDeviceProfileByNameRoute     = ApiDeviceProfileRoute + "/" + Name + "/{" + Name + "}"
+	ApiDeviceRoute                  = ApiBase + "/device"
+	ApiDeviceServiceRoute           = ApiBase + "/deviceservice"
+	ApiConfigRoute                  = ApiBase + "/config"
+	ApiMetricsRoute                 = ApiBase + "/metrics"
+	ApiPingRoute                    = ApiBase + "/ping"
+	ApiVersionRoute                 = ApiBase + "/version"
 )
 
 // Constants related to defined url path names and parameters in the v2 service APIs
@@ -49,4 +51,5 @@ const (
 	Age           = "age"
 	Scrub         = "scrub"
 	Type          = "type"
+	Name          = "name"
 )
