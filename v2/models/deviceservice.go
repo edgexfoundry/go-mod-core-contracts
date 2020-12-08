85d4d6a2a0daf81ba27e7762990535f8af05f231
weichou <weichou1229@gmail.com>
2020-12-08 15:41:22 +0800

    refactor(meta): Remove unnecessary comments
    Fix #318
    
    Signed-off-by: weichou <weichou1229@gmail.com>

--- v2/models/deviceservice.go
+++ v2/models/deviceservice.go
@@ -11,11 +11,11 @@ package models
 type DeviceService struct {
 	Timestamps
 	Id            string
-	Name          string     // time in milliseconds that the device last provided any feedback or responded to any request
-	Description   string     // Description of the device service
-	LastConnected int64      // time in milliseconds that the device last reported data to the core
-	LastReported  int64      // operational state - either enabled or disabled
-	Labels        []string   // tags or other labels applied to the device service for search or other identification needs
-	BaseAddress   string     // BaseAddress is a fully qualified URI, e.g. <protocol>:\\<hostname>:<port>/<optional path>
-	AdminState    AdminState // Device Service Admin State
+	Name          string
+	Description   string
+	LastConnected int64
+	LastReported  int64
+	Labels        []string
+	BaseAddress   string
+	AdminState    AdminState
 }
