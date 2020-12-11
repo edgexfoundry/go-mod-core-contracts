db0762886a4ec0c16db37ea69e7caa504dbbcf6f
weichou <weichou1229@gmail.com>
2020-12-11 14:19:59 +0800

    feat(meta): Add V2 client for querying device profile
    Add V2 client for querying the device profile
    - query by name
    - query all
    - query all by model
    - query all by manufacturer
    - query all by manufacturer and model
    
    Fix #392
    
    Signed-off-by: weichou <weichou1229@gmail.com>

--- v2/clients/http/deviceprofile.go
+++ v2/clients/http/deviceprofile.go
@@ -9,6 +9,10 @@ import (
 	"context"
 	"net/url"
 	"path"
+	"net/url"
+	"path"
+	"strconv"
+	"strings"
 
 	"github.com/edgexfoundry/go-mod-core-contracts/errors"
 	"github.com/edgexfoundry/go-mod-core-contracts/v2"
@@ -16,6 +20,7 @@ import (
 	"github.com/edgexfoundry/go-mod-core-contracts/v2/clients/interfaces"
 	"github.com/edgexfoundry/go-mod-core-contracts/v2/dtos/common"
 	"github.com/edgexfoundry/go-mod-core-contracts/v2/dtos/requests"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/dtos/responses"
 )
 
 type DeviceProfileClient struct {
@@ -77,4 +82,88 @@ func (client *DeviceProfileClient) DeleteByName(ctx context.Context, name string
 		return response, errors.NewCommonEdgeXWrapper(err)
 	}
 	return response, nil
+}
+
+func (client *DeviceProfileClient) DeviceProfileByName(ctx context.Context, name string) (res responses.DeviceProfileResponse, edgexError errors.EdgeX) {
+	u, err := url.Parse(client.baseUrl)
+	if err != nil {
+		return res, errors.NewCommonEdgeX(errors.KindClientError, "fail to parse baseUrl", err)
+	}
+	u.Path = path.Join(u.Path, v2.ApiDeviceProfileRoute, v2.Name, url.QueryEscape(name))
+	err = utils.GetRequest(ctx, &res, u.String())
+	if err != nil {
+		return res, errors.NewCommonEdgeXWrapper(err)
+	}
+	return res, nil
+}
+
+func (client *DeviceProfileClient) AllDeviceProfiles(ctx context.Context, labels []string, offset int, limit int) (res responses.MultiDeviceProfilesResponse, edgexError errors.EdgeX) {
+	u, err := url.Parse(client.baseUrl)
+	if err != nil {
+		return res, errors.NewCommonEdgeX(errors.KindClientError, "fail to parse baseUrl", err)
+	}
+	u.Path = path.Join(u.Path, v2.ApiAllDeviceProfileRoute)
+	q := u.Query()
+	if len(labels) > 0 {
+		q.Set(v2.Labels, strings.Join(labels, v2.CommaSeparator))
+	}
+	q.Set(v2.Offset, strconv.Itoa(offset))
+	q.Set(v2.Limit, strconv.Itoa(limit))
+	u.RawQuery = q.Encode()
+	err = utils.GetRequest(ctx, &res, u.String())
+	if err != nil {
+		return res, errors.NewCommonEdgeXWrapper(err)
+	}
+	return res, nil
+}
+
+func (client *DeviceProfileClient) DeviceProfilesByModel(ctx context.Context, model string, offset int, limit int) (res responses.MultiDeviceProfilesResponse, edgexError errors.EdgeX) {
+	u, err := url.Parse(client.baseUrl)
+	if err != nil {
+		return res, errors.NewCommonEdgeX(errors.KindClientError, "fail to parse baseUrl", err)
+	}
+	u.Path = path.Join(u.Path, v2.ApiDeviceProfileRoute, v2.Model, url.QueryEscape(model))
+	q := u.Query()
+	q.Set(v2.Offset, strconv.Itoa(offset))
+	q.Set(v2.Limit, strconv.Itoa(limit))
+	u.RawQuery = q.Encode()
+	err = utils.GetRequest(ctx, &res, u.String())
+	if err != nil {
+		return res, errors.NewCommonEdgeXWrapper(err)
+	}
+	return res, nil
+}
+
+func (client *DeviceProfileClient) DeviceProfilesByManufacturer(ctx context.Context, manufacturer string, offset int, limit int) (res responses.MultiDeviceProfilesResponse, edgexError errors.EdgeX) {
+	u, err := url.Parse(client.baseUrl)
+	if err != nil {
+		return res, errors.NewCommonEdgeX(errors.KindClientError, "fail to parse baseUrl", err)
+	}
+	u.Path = path.Join(u.Path, v2.ApiDeviceProfileRoute, v2.Manufacturer, url.QueryEscape(manufacturer))
+	q := u.Query()
+	q.Set(v2.Offset, strconv.Itoa(offset))
+	q.Set(v2.Limit, strconv.Itoa(limit))
+	u.RawQuery = q.Encode()
+	err = utils.GetRequest(ctx, &res, u.String())
+	if err != nil {
+		return res, errors.NewCommonEdgeXWrapper(err)
+	}
+	return res, nil
+}
+
+func (client *DeviceProfileClient) DeviceProfilesByManufacturerAndModel(ctx context.Context, manufacturer string, model string, offset int, limit int) (res responses.MultiDeviceProfilesResponse, edgexError errors.EdgeX) {
+	u, err := url.Parse(client.baseUrl)
+	if err != nil {
+		return res, errors.NewCommonEdgeX(errors.KindClientError, "fail to parse baseUrl", err)
+	}
+	u.Path = path.Join(u.Path, v2.ApiDeviceProfileRoute, v2.Manufacturer, url.QueryEscape(manufacturer), v2.Model, url.QueryEscape(model))
+	q := u.Query()
+	q.Set(v2.Offset, strconv.Itoa(offset))
+	q.Set(v2.Limit, strconv.Itoa(limit))
+	u.RawQuery = q.Encode()
+	err = utils.GetRequest(ctx, &res, u.String())
+	if err != nil {
+		return res, errors.NewCommonEdgeXWrapper(err)
+	}
+	return res, nil
 }
