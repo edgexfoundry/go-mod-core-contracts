b476394796122ddc0b37068ad6a2ab0c44707788
Felix Ting <felix@iotechsys.com>
2021-01-04 20:34:30 +0800

    feat(meta): Complete the EventClient
    Complete the V2 EventClient for interactions with the V2 Event endpoint on the Core Data Service.
    - (POST) /event
    - (GET) /event/all
    - (GET) /event/count
    - (GET) /event/count/device/name/{name}
    - (GET) /event/device/name/{name}
    - (DELETE) /event/device/name/{name}
    - (GET) /event/start/{start}/end/{start}
    - (DELETE) /event/age/{age}
    
    Signed-off-by: Felix Ting <felix@iotechsys.com>

--- v2/clients/http/event_test.go
+++ v2/clients/http/event_test.go
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2020 IOTech Ltd
+// Copyright (C) 2021 IOTech Ltd
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -7,42 +7,107 @@ package http
 
 import (
 	"context"
-	"encoding/json"
 	"net/http"
-	"net/http/httptest"
+	"path"
+	"strconv"
 	"testing"
 
 	"github.com/edgexfoundry/go-mod-core-contracts/v2"
 	"github.com/edgexfoundry/go-mod-core-contracts/v2/dtos/common"
 	"github.com/edgexfoundry/go-mod-core-contracts/v2/dtos/requests"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/dtos/responses"
 
-	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestAddEvent(t *testing.T) {
-	requestId := uuid.New().String()
-
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
-		}
-		if r.URL.EscapedPath() != v2.ApiEventRoute {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		w.WriteHeader(http.StatusMultiStatus)
-		br := common.NewBaseWithIdResponse(requestId, "", http.StatusMultiStatus, uuid.New().String())
-		res, _ := json.Marshal([]common.BaseWithIdResponse{br})
-		_, _ = w.Write(res)
-	}))
+	ts := newTestServer(http.MethodPost, v2.ApiEventRoute, []common.BaseWithIdResponse{})
 	defer ts.Close()
 
-	ec := NewEventClient(ts.URL)
-	res, err := ec.Add(context.Background(), []requests.AddEventRequest{})
+	client := NewEventClient(ts.URL)
+	res, err := client.Add(context.Background(), []requests.AddEventRequest{})
 	require.NoError(t, err)
-	require.NotNil(t, res)
-	require.Equal(t, requestId, res[0].RequestId)
+	assert.IsType(t, []common.BaseWithIdResponse{}, res)
+}
+
+func TestQueryAllEvents(t *testing.T) {
+	ts := newTestServer(http.MethodGet, v2.ApiAllEventRoute, responses.MultiEventsResponse{})
+	defer ts.Close()
+
+	client := NewEventClient(ts.URL)
+	res, err := client.AllEvents(context.Background(), 1, 10)
+	require.NoError(t, err)
+	assert.IsType(t, responses.MultiEventsResponse{}, res)
+}
+
+func TestQueryEventCount(t *testing.T) {
+	ts := newTestServer(http.MethodGet, v2.ApiEventCountRoute, common.CountResponse{})
+	defer ts.Close()
+
+	client := NewEventClient(ts.URL)
+	res, err := client.EventCount(context.Background())
+	require.NoError(t, err)
+	assert.IsType(t, common.CountResponse{}, res)
+}
+
+func TestQueryEventCountByDeviceName(t *testing.T) {
+	deviceName := "device"
+	path := path.Join(v2.ApiEventCountRoute, v2.Device, v2.Name, deviceName)
+	ts := newTestServer(http.MethodGet, path, common.CountResponse{})
+	defer ts.Close()
+
+	client := NewEventClient(ts.URL)
+	res, err := client.EventCountByDeviceName(context.Background(), deviceName)
+	require.NoError(t, err)
+	require.IsType(t, common.CountResponse{}, res)
+}
+
+func TestQueryEventsByDeviceName(t *testing.T) {
+	deviceName := "device"
+	urlPath := path.Join(v2.ApiEventRoute, v2.Device, v2.Name, deviceName)
+	ts := newTestServer(http.MethodGet, urlPath, responses.MultiEventsResponse{})
+	defer ts.Close()
+
+	client := NewEventClient(ts.URL)
+	res, err := client.EventsByDeviceName(context.Background(), deviceName, 1, 10)
+	require.NoError(t, err)
+	assert.IsType(t, responses.MultiEventsResponse{}, res)
+}
+
+func TestDeleteEventsByDeviceName(t *testing.T) {
+	deviceName := "device"
+	path := path.Join(v2.ApiEventRoute, v2.Device, v2.Name, deviceName)
+	ts := newTestServer(http.MethodDelete, path, common.BaseResponse{})
+	defer ts.Close()
+
+	client := NewEventClient(ts.URL)
+	res, err := client.DeleteByDeviceName(context.Background(), deviceName)
+	require.NoError(t, err)
+	assert.IsType(t, common.BaseResponse{}, res)
+}
+
+func TestQueryEventsByTimeRange(t *testing.T) {
+	start := 1
+	end := 10
+	urlPath := path.Join(v2.ApiEventRoute, v2.Start, strconv.Itoa(start), v2.End, strconv.Itoa(end))
+	ts := newTestServer(http.MethodGet, urlPath, responses.MultiEventsResponse{})
+	defer ts.Close()
+
+	client := NewEventClient(ts.URL)
+	res, err := client.EventsByTimeRange(context.Background(), start, end, 1, 10)
+	require.NoError(t, err)
+	assert.IsType(t, responses.MultiEventsResponse{}, res)
+}
+
+func TestDeleteEventsByAge(t *testing.T) {
+	age := 10
+	path := path.Join(v2.ApiEventRoute, v2.Age, strconv.Itoa(age))
+	ts := newTestServer(http.MethodDelete, path, common.BaseResponse{})
+	defer ts.Close()
+
+	client := NewEventClient(ts.URL)
+	res, err := client.DeleteByAge(context.Background(), age)
+	require.NoError(t, err)
+	assert.IsType(t, common.BaseResponse{}, res)
 }
