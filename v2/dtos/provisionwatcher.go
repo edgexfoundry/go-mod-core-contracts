50cbdd60c68c4034b5ed66b142a4e768b8054292
Chris Hung <chris@iotechsys.com>
2021-01-06 12:04:57 +0800

    feat(meta): create ProvisionWatcher Model and DTO
    Signed-off-by: Chris Hung <chris@iotechsys.com>

---
+++ v2/dtos/provisionwatcher.go
@@ -0,0 +1,72 @@
+//
+// Copyright (C) 2021 IOTech Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package dtos
+
+import (
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/dtos/common"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/models"
+)
+
+// ProvisionWatcher and its properties are defined in the APIv2 specification:
+// https://app.swaggerhub.com/apis-docs/EdgeXFoundry1/core-metadata/2.x#/ProvisionWatcher
+type ProvisionWatcher struct {
+	common.Versionable  `json:",inline"`
+	Id                  string              `json:"id,omitempty" validate:"omitempty,uuid"`
+	Name                string              `json:"name" validate:"required,edgex-dto-none-empty-string"`
+	Labels              []string            `json:"labels,omitempty"`
+	Identifiers         map[string]string   `json:"identifiers" validate:"gt=0,dive,keys,required,endkeys,required"`
+	BlockingIdentifiers map[string][]string `json:"blockingIdentifiers,omitempty"`
+	ProfileName         string              `json:"profile" validate:"required,edgex-dto-none-empty-string"`
+	ServiceName         string              `json:"service" validate:"required,edgex-dto-none-empty-string"`
+	AdminState          string              `json:"adminState" validate:"oneof='LOCKED' 'UNLOCKED'"`
+	AutoEvents          []AutoEvent         `json:"autoEvents,omitempty" validate:"dive"`
+}
+
+// UpdateProvisionWatcher and its properties are defined in the APIv2 specification:
+// https://app.swaggerhub.com/apis-docs/EdgeXFoundry1/core-metadata/2.x#/UpdateProvisionWatcher
+type UpdateProvisionWatcher struct {
+	common.Versionable  `json:",inline"`
+	Id                  *string             `json:"id" validate:"required_without=Name,edgex-dto-uuid"`
+	Name                *string             `json:"name" validate:"required_without=Id,edgex-dto-none-empty-string"`
+	Labels              []string            `json:"labels"`
+	Identifiers         map[string]string   `json:"identifiers" validate:"omitempty,gt=0,dive,keys,required,endkeys,required"`
+	BlockingIdentifiers map[string][]string `json:"blockingIdentifiers"`
+	ProfileName         *string             `json:"profileName" validate:"omitempty,edgex-dto-none-empty-string"`
+	ServiceName         *string             `json:"serviceName" validate:"omitempty,edgex-dto-none-empty-string"`
+	AdminState          *string             `json:"adminState" validate:"omitempty,oneof='LOCKED' 'UNLOCKED'"`
+	AutoEvents          []AutoEvent         `json:"autoEvents" validate:"dive"`
+}
+
+// ToProvisionWatcherModel transforms the ProvisionWatcher DTO to the ProvisionWatcher model
+func ToProvisionWatcherModel(dto ProvisionWatcher) models.ProvisionWatcher {
+	return models.ProvisionWatcher{
+		Timestamps:          models.Timestamps{},
+		Id:                  dto.Id,
+		Name:                dto.Name,
+		Labels:              dto.Labels,
+		Identifiers:         dto.Identifiers,
+		BlockingIdentifiers: dto.BlockingIdentifiers,
+		ProfileName:         dto.ProfileName,
+		ServiceName:         dto.ServiceName,
+		AdminState:          models.AdminState(dto.AdminState),
+		AutoEvents:          ToAutoEventModels(dto.AutoEvents),
+	}
+}
+
+// FromProvisionWatcherModelToDTO transforms the ProvisionWatcher Model to the ProvisionWatcher DTO
+func FromProvisionWatcherModelToDTO(pw models.ProvisionWatcher) ProvisionWatcher {
+	return ProvisionWatcher{
+		Id:                  pw.Id,
+		Name:                pw.Name,
+		Labels:              pw.Labels,
+		Identifiers:         pw.Identifiers,
+		BlockingIdentifiers: pw.BlockingIdentifiers,
+		ProfileName:         pw.ProfileName,
+		ServiceName:         pw.ServiceName,
+		AdminState:          string(pw.AdminState),
+		AutoEvents:          FromAutoEventModelsToDTOs(pw.AutoEvents),
+	}
+}
---
+++ v2/dtos/responses/provisionwatcher.go
@@ -0,0 +1,41 @@
+//
+// Copyright (C) 2021 IOTech Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package responses
+
+import (
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/dtos"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/dtos/common"
+)
+
+// ProvisionWatcherResponse defines the Response Content for GET ProvisionWatcher DTOs.
+// This object and its properties correspond to the ProvisionWatcherResponse object in the APIv2 specification:
+// https://app.swaggerhub.com/apis-docs/EdgeXFoundry1/core-metadata/2.x#/ProvisionWatcherResponse
+type ProvisionWatcherResponse struct {
+	common.BaseResponse `json:",inline"`
+	ProvisionWatcher    dtos.ProvisionWatcher `json:"provisionWatcher"`
+}
+
+func NewProvisionWatcherResponse(requestId string, message string, statusCode int, pw dtos.ProvisionWatcher) ProvisionWatcherResponse {
+	return ProvisionWatcherResponse{
+		BaseResponse:     common.NewBaseResponse(requestId, message, statusCode),
+		ProvisionWatcher: pw,
+	}
+}
+
+// MultiProvisionWatchersResponse defines the Response Content for GET multiple ProvisionWatcher DTOs.
+// This object and its properties correspond to the MultiProvisionWatchersResponse object in the APIv2 specification:
+// https://app.swaggerhub.com/apis-docs/EdgeXFoundry1/core-metadata/2.x#/MultiProvisionWatchersResponse
+type MultiProvisionWatchersResponse struct {
+	common.BaseResponse `json:",inline"`
+	ProvisionWatchers   []dtos.ProvisionWatcher `json:"provisionWatchers"`
+}
+
+func NewMultiProvisionWatchersResponse(requestId string, message string, statusCode int, pws []dtos.ProvisionWatcher) MultiProvisionWatchersResponse {
+	return MultiProvisionWatchersResponse{
+		BaseResponse:      common.NewBaseResponse(requestId, message, statusCode),
+		ProvisionWatchers: pws,
+	}
+}
