6e7f6a6a49e222f1beb89096e172e328fea800e0
Felix Ting <felix@iotechsys.com>
2021-01-18 19:21:42 +0800

    feat(notifications): Create Subscription DTO and Model
    Create Subscription DTO and Model for Notifications V2 API
    
    Signed-off-by: Felix Ting <felix@iotechsys.com>

---
+++ v2/dtos/responses/subscription_test.go
@@ -0,0 +1,43 @@
+//
+// Copyright (C) 2020-2021 IOTech Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package responses
+
+import (
+	"testing"
+
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/v2/dtos"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewSubscriptionResponse(t *testing.T) {
+	expectedRequestId := "123456"
+	expectedStatusCode := 200
+	expectedMessage := "unit test message"
+	expectedSubscription := dtos.Subscription{Name: "test Subscription"}
+	actual := NewSubscriptionResponse(expectedRequestId, expectedMessage, expectedStatusCode, expectedSubscription)
+
+	assert.Equal(t, expectedRequestId, actual.RequestId)
+	assert.Equal(t, expectedStatusCode, actual.StatusCode)
+	assert.Equal(t, expectedMessage, actual.Message)
+	assert.Equal(t, expectedSubscription, actual.Subscription)
+}
+
+func TestNewMultiSubscriptionsResponse(t *testing.T) {
+	expectedRequestId := "123456"
+	expectedStatusCode := 200
+	expectedMessage := "unit test message"
+	expectedSubscriptions := []dtos.Subscription{
+		{Name: "test Subscription1"},
+		{Name: "test Subscription2"},
+	}
+	actual := NewMultiSubscriptionsResponse(expectedRequestId, expectedMessage, expectedStatusCode, expectedSubscriptions)
+
+	assert.Equal(t, expectedRequestId, actual.RequestId)
+	assert.Equal(t, expectedStatusCode, actual.StatusCode)
+	assert.Equal(t, expectedMessage, actual.Message)
+	assert.Equal(t, expectedSubscriptions, actual.Subscriptions)
+}
---
+++ v2/dtos/subscription.go
@@ -0,0 +1,154 @@
+//
+// Copyright (C) 2020-2021 IOTech Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package dtos
+
+import (
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/v2/dtos/common"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/v2/models"
+)
+
+// Subscription and its properties are defined in the APIv2 specification:
+// https://app.swaggerhub.com/apis-docs/EdgeXFoundry1/support-notifications/2.x#/Subscription
+type Subscription struct {
+	common.Versionable `json:",inline"`
+	Id                 string    `json:"id" validate:"omitempty,uuid"`
+	Name               string    `json:"name" validate:"required,edgex-dto-none-empty-string,edgex-dto-rfc3986-unreserved-chars"`
+	Channels           []Channel `json:"channels" validate:"required,gt=0,dive"`
+	Receiver           string    `json:"receiver" validate:"required,edgex-dto-none-empty-string,edgex-dto-rfc3986-unreserved-chars"`
+	Categories         []string  `json:"categories" validate:"omitempty,required_without=Labels,gt=0,dive,oneof='SECURITY' 'SW_HEALTH' 'HW_HEALTH'"`
+	Labels             []string  `json:"labels" validate:"omitempty,required_without=Categories,gt=0,dive,edgex-dto-none-empty-string,edgex-dto-rfc3986-unreserved-chars"`
+	Created            int64     `json:"created,omitempty"`
+	Modified           int64     `json:"modified,omitempty"`
+	Description        string    `json:"description,omitempty"`
+	ResendLimit        int64     `json:"resendLimit,omitempty"`
+	ResendInterval     string    `json:"resendInterval" validate:"omitempty,edgex-dto-frequency"`
+}
+
+// UpdateSubscription and its properties are defined in the APIv2 specification:
+// https://app.swaggerhub.com/apis-docs/EdgeXFoundry1/support-notifications/2.x#/UpdateSubscription
+type UpdateSubscription struct {
+	common.Versionable `json:",inline"`
+	Id                 *string   `json:"id" validate:"omitempty,required_without=Name,uuid"`
+	Name               *string   `json:"name" validate:"omitempty,required_without=Id,edgex-dto-none-empty-string,edgex-dto-rfc3986-unreserved-chars"`
+	Channels           []Channel `json:"channels" validate:"omitempty,dive"`
+	Receiver           *string   `json:"receiver" validate:"omitempty,edgex-dto-none-empty-string,edgex-dto-rfc3986-unreserved-chars"`
+	Categories         []string  `json:"categories" validate:"omitempty,dive,oneof='SECURITY' 'SW_HEALTH' 'HW_HEALTH'"`
+	Labels             []string  `json:"labels" validate:"omitempty,dive,edgex-dto-none-empty-string,edgex-dto-rfc3986-unreserved-chars"`
+	Description        *string   `json:"description" validate:"omitempty"`
+	ResendLimit        *int64    `json:"resendLimit" validate:"omitempty"`
+	ResendInterval     *string   `json:"resendInterval" validate:"omitempty,edgex-dto-frequency"`
+}
+
+// Channel and its properties are defined in the APIv2 specification:
+// https://app.swaggerhub.com/apis-docs/EdgeXFoundry1/support-notifications/2.x#/Channel
+type Channel struct {
+	Type           string   `json:"type" validate:"required,oneof='REST' 'EMAIL'"`
+	EmailAddresses []string `json:"emailAddresses" validate:"omitempty,required_without=Url,gt=0,dive,email"`
+	Url            string   `json:"url" validate:"omitempty,required_without=EmailAddresses,uri"`
+}
+
+// ToSubscriptionModel transforms the Subscription DTO to the Subscription Model
+func ToSubscriptionModel(s Subscription) models.Subscription {
+	var m models.Subscription
+	m.Categories = ToCategoryModels(s.Categories)
+	m.Labels = s.Labels
+	m.Channels = ToChannelModels(s.Channels)
+	m.Created = s.Created
+	m.Modified = s.Modified
+	m.Description = s.Description
+	m.Id = s.Id
+	m.Receiver = s.Receiver
+	m.Name = s.Name
+	m.ResendLimit = s.ResendLimit
+	m.ResendInterval = s.ResendInterval
+	return m
+}
+
+// ToSubscriptionModels transforms the Subscription DTO array to the Subscription model array
+func ToSubscriptionModels(subs []Subscription) []models.Subscription {
+	models := make([]models.Subscription, len(subs))
+	for i, s := range subs {
+		models[i] = ToSubscriptionModel(s)
+	}
+	return models
+}
+
+// FromSubscriptionModelToDTO transforms the Subscription Model to the Subscription DTO
+func FromSubscriptionModelToDTO(s models.Subscription) Subscription {
+	return Subscription{
+		Categories:     FromCategoryModelsToStrings(s.Categories),
+		Labels:         s.Labels,
+		Channels:       FromChannelModelsToDTOs(s.Channels),
+		Created:        s.Created,
+		Modified:       s.Modified,
+		Description:    s.Description,
+		Id:             s.Id,
+		Receiver:       s.Receiver,
+		Name:           s.Name,
+		ResendLimit:    s.ResendLimit,
+		ResendInterval: s.ResendInterval,
+	}
+}
+
+// FromSubscriptionModels transforms the Subscription model array to the Subscription DTO array
+func FromSubscriptionModelsToDTOs(subscruptions []models.Subscription) []Subscription {
+	dtos := make([]Subscription, len(subscruptions))
+	for i, s := range subscruptions {
+		dtos[i] = FromSubscriptionModelToDTO(s)
+	}
+	return dtos
+}
+
+func ToChannelModels(channelDTOs []Channel) []models.Channel {
+	channelModels := make([]models.Channel, len(channelDTOs))
+	for i, c := range channelDTOs {
+		channelModels[i] = ToChannelModel(c)
+	}
+	return channelModels
+}
+
+func ToChannelModel(c Channel) models.Channel {
+	return models.Channel{
+		Type:           models.ChannelType(c.Type),
+		EmailAddresses: c.EmailAddresses,
+		Url:            c.Url,
+	}
+}
+
+// FromChannelModelsToDTOs transforms the Channel model array to the Channel DTO array
+func FromChannelModelsToDTOs(cs []models.Channel) []Channel {
+	dtos := make([]Channel, len(cs))
+	for i, c := range cs {
+		dtos[i] = FromChannelModelToDTO(c)
+	}
+	return dtos
+}
+
+// FromChannelModelToDTO transforms the Channel model to the Channel DTO
+func FromChannelModelToDTO(c models.Channel) Channel {
+	return Channel{
+		Type:           string(c.Type),
+		EmailAddresses: c.EmailAddresses,
+		Url:            c.Url,
+	}
+}
+
+func ToCategoryModels(categories []string) []models.Category {
+	categoryModels := make([]models.Category, len(categories))
+	for i, c := range categories {
+		categoryModels[i] = models.Category(c)
+	}
+	return categoryModels
+}
+
+// FromCategoryModelsToDTOs transforms the Category model array to string array
+func FromCategoryModelsToStrings(cs []models.Category) []string {
+	s := make([]string, len(cs))
+	for i, c := range cs {
+		s[i] = string(c)
+	}
+	return s
+}
