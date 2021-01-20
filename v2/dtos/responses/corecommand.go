2037e559c41eb03dfb875a692e4ba285bba6a7cb
Jude Hung <jude@iotechsys.com>
2021-01-20 15:21:42 +0800

    feat(command): Revise CommandResponse DTO to MultiCoreCommandsResponse
    1. Create and rename CommandResposne DTO to MultiCoreCommandsResponse
    2. Create CoreCommand DTO
    
    Signed-off-by: Jude Hung <jude@iotechsys.com>

---
+++ v2/dtos/responses/corecommand.go
@@ -0,0 +1,26 @@
+//
+// Copyright (C) 2021 IOTech Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package responses
+
+import (
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/dtos"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/dtos/common"
+)
+
+// MultiCoreCommandsResponse defines the Response Content for GET multiple CoreCommand DTOs.
+// This object and its properties correspond to the MultiCoreCommandsResponse object in the APIv2 specification:
+// https://app.swaggerhub.com/apis-docs/EdgeXFoundry1/core-metadata/2.x#/MultiCoreCommandsResponse
+type MultiCoreCommandsResponse struct {
+	common.BaseResponse `json:",inline"`
+	CoreCommands        []dtos.CoreCommand `json:"coreCommands"`
+}
+
+func NewMultiCoreCommandsResponse(requestId string, message string, statusCode int, commands []dtos.CoreCommand) MultiCoreCommandsResponse {
+	return MultiCoreCommandsResponse{
+		BaseResponse: common.NewBaseResponse(requestId, message, statusCode),
+		CoreCommands: commands,
+	}
+}
