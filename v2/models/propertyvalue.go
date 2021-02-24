bfd2809b8f5511e85e0f1f712ef9f57d73f60e02
weichou <weichou1229@gmail.com>
2021-02-24 13:24:58 +0800

    refactor(v2): Remove encoding constants
    Per Core WG review of 2/18/21, remove the encoding constants.
    
    Close #511
    
    Signed-off-by: weichou <weichou1229@gmail.com>

--- v2/models/propertyvalue.go
+++ v2/models/propertyvalue.go
@@ -5,13 +5,6 @@
 
 package models
 
-const (
-	// Base64Encoding : the float value is represented in Base64 encoding
-	Base64Encoding = "Base64"
-	// ENotation : the float value is represented in eNotation
-	ENotation = "eNotation"
-)
-
 // PropertyValue and its properties care defined in the APIv2 specification:
 // https://app.swaggerhub.com/apis-docs/EdgeXFoundry1/core-metadata/2.x#/PropertyValue
 // Model fields are same as the DTOs documented by this swagger. Exceptions, if any, are noted below.
