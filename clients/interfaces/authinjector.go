22d5e16a8980b24fc88809bb5198de467db1a9fe
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-02-28 09:55:14 -0500

    feat: add openziti to core contracts (#876)
    * feat: add openziti to core contracts
    
    Signed-off-by: dovholuknf <46322585+dovholuknf@users.noreply.github.com>
    
    * fix: allow unit tests to pass by satisfying the interface
    
    Signed-off-by: dovholuknf <46322585+dovholuknf@users.noreply.github.com>
    
    * fix: remove unused file, committed by mistake
    
    Signed-off-by: dovholuknf <46322585+dovholuknf@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: dovholuknf <46322585+dovholuknf@users.noreply.github.com>

--- clients/interfaces/authinjector.go
+++ clients/interfaces/authinjector.go
@@ -9,9 +9,17 @@ import (
 	"net/http"
 )
 
-// AuthenticationInjector defines an interface to obtain a JWT for remote service calls
+// AuthenticationInjector defines an interface to obtain a JWT and secure transport for remote service calls
 type AuthenticationInjector interface {
 	// AddAuthenticationData mutates an HTTP request to add authentication data
 	// (suth as an Authorization: header) to an outbound HTTP request
 	AddAuthenticationData(_ *http.Request) error
+	// Returns the configured *http.Transport to use when making the request
+	SecureTransportProvider
+}
+
+// SecureTransportProvider defines an interface to obtain a secure http.Transport to use when making http requests
+type SecureTransportProvider interface {
+	// Returns the configured *http.Transport to use when making the request
+	RoundTripper() http.RoundTripper
 }
