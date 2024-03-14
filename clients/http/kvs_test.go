e3dd7029928a0a6ca0433a8ff353d6de7cdd76df
Jack Chen <jack@iotechsys.com>
2024-03-14 14:59:03 +0800

    feat: Add the http clients/dtos of KVS and registry for Keeper
    Signed-off-by: Jack Chen <jack@iotechsys.com>

---
+++ clients/http/kvs_test.go
@@ -0,0 +1,75 @@
+//
+// Copyright (C) 2024 IOTech Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package http
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/edgexfoundry/go-mod-core-contracts/v3/common"
+	"github.com/edgexfoundry/go-mod-core-contracts/v3/dtos/requests"
+	"github.com/edgexfoundry/go-mod-core-contracts/v3/dtos/responses"
+
+	"github.com/stretchr/testify/require"
+)
+
+const TestKey = "TestWritable"
+
+func TestUpdateValuesByKey(t *testing.T) {
+	ts := newTestServer(http.MethodPut, common.ApiKVSRoute+"/"+common.Key+"/"+TestKey, responses.KeysResponse{})
+	defer ts.Close()
+
+	client := NewKVSClient(ts.URL, NewNullAuthenticationInjector())
+	res, err := client.UpdateValuesByKey(context.Background(), TestKey, requests.UpdateKeysRequest{})
+
+	require.NoError(t, err)
+	require.IsType(t, responses.KeysResponse{}, res)
+}
+
+func TestValuesByKey(t *testing.T) {
+	ts := newTestServer(http.MethodGet, common.ApiKVSRoute+"/"+common.Key+"/"+TestKey, responses.MultiKeyValueResponse{})
+	defer ts.Close()
+
+	client := NewKVSClient(ts.URL, NewNullAuthenticationInjector())
+	res, err := client.ValuesByKey(context.Background(), TestKey)
+
+	require.NoError(t, err)
+	require.IsType(t, responses.MultiKeyValueResponse{}, res)
+}
+
+func TestListKeys(t *testing.T) {
+	ts := newTestServer(http.MethodGet, common.ApiKVSRoute+"/"+common.Key+"/"+TestKey, responses.KeysResponse{})
+	defer ts.Close()
+
+	client := NewKVSClient(ts.URL, NewNullAuthenticationInjector())
+	res, err := client.ListKeys(context.Background(), TestKey)
+
+	require.NoError(t, err)
+	require.IsType(t, responses.KeysResponse{}, res)
+}
+
+func TestDeleteKeys(t *testing.T) {
+	ts := newTestServer(http.MethodDelete, common.ApiKVSRoute+"/"+common.Key+"/"+TestKey, responses.KeysResponse{})
+	defer ts.Close()
+
+	client := NewKVSClient(ts.URL, NewNullAuthenticationInjector())
+	res, err := client.DeleteKey(context.Background(), TestKey)
+
+	require.NoError(t, err)
+	require.IsType(t, responses.KeysResponse{}, res)
+}
+
+func TestDeleteKeysByPrefix(t *testing.T) {
+	ts := newTestServer(http.MethodDelete, common.ApiKVSRoute+"/"+common.Key+"/"+TestKey, responses.KeysResponse{})
+	defer ts.Close()
+
+	client := NewKVSClient(ts.URL, NewNullAuthenticationInjector())
+	res, err := client.DeleteKeysByPrefix(context.Background(), TestKey)
+
+	require.NoError(t, err)
+	require.IsType(t, responses.KeysResponse{}, res)
+}
---
+++ dtos/responses/kvs_test.go
@@ -0,0 +1,48 @@
+//
+// Copyright (C) 2024 IOTech Ltd
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package responses
+
+import (
+	"testing"
+
+	"github.com/edgexfoundry/go-mod-core-contracts/v3/models"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	expectedRequestId  = "123456"
+	expectedStatusCode = 200
+	expectedMessage    = "unit test message"
+	expectedTestKey    = "TestKey"
+)
+
+func TestNewMultiKVResponse(t *testing.T) {
+	expectedKV := models.KVS{
+		Key: expectedTestKey,
+		StoredData: models.StoredData{
+			DBTimestamp: models.DBTimestamp{},
+			Value:       "TestValue",
+		},
+	}
+	expectedResp := []models.KVResponse{&expectedKV}
+	actual := NewMultiKVResponse(expectedRequestId, expectedMessage, expectedStatusCode, expectedResp)
+
+	assert.Equal(t, expectedRequestId, actual.RequestId)
+	assert.Equal(t, expectedStatusCode, actual.StatusCode)
+	assert.Equal(t, expectedMessage, actual.Message)
+	assert.Equal(t, expectedResp, actual.Response)
+}
+
+func TestNewKeysResponse(t *testing.T) {
+	expectedResp := []models.KeyOnly{expectedTestKey}
+	actual := NewKeysResponse(expectedRequestId, expectedMessage, expectedStatusCode, expectedResp)
+
+	assert.Equal(t, expectedRequestId, actual.RequestId)
+	assert.Equal(t, expectedStatusCode, actual.StatusCode)
+	assert.Equal(t, expectedMessage, actual.Message)
+	assert.Equal(t, expectedResp, actual.Response)
+}
