5a5df9f24fe6bb589cf43320871217be97e4628e
Lindsey Cheng <beckysocute@gmail.com>
2025-08-11 10:38:24 +0800

    feat: Support day unit with edgex-dto-duration validation tag (#1014)
    * feat: Support day unit in edgex-dto-duration validation tag
    
    Resolves #1013. Support day unit in edgex-dto-duration validation tag.
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>
    
    * fix: Handle repeated days in duration string
    
    Handle repeated days in duration string.
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>
    
    ---------
    
    Signed-off-by: Lindsey Cheng <beckysocute@gmail.com>

--- common/validator.go
+++ common/validator.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"reflect"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
@@ -135,38 +136,90 @@ func getErrorMessage(e validator.FieldError) string {
 // ex. edgex-dto-duration=10ms0x2C24h - 10ms represents the minimum Duration and 24h represents the maximum Duration
 // 0x2c is the UTF-8 hex encoding of comma (,) as the min/max value separator
 func ValidateDuration(fl validator.FieldLevel) bool {
-	duration, err := time.ParseDuration(fl.Field().String())
-	if err != nil {
+	durationStr := fl.Field().String()
+
+	valid, duration := parseDurationWithDay(durationStr)
+	if !valid {
 		return false
 	}
 
 	// if min/max are defined from tag param, check if the duration value is in the duration range
 	param := fl.Param()
-	var min, max time.Duration
 	if param != "" {
 		params := strings.Split(param, CommaSeparator)
 		if len(params) > 0 {
-			min, err = time.ParseDuration(params[0])
-			if err != nil {
-				return false
-			}
-			if duration < min {
-				// the duration value is smaller than the min
-				return false
-			}
-			if len(params) > 1 {
-				max, err = time.ParseDuration(params[1])
-				if err != nil {
+			// Check if minimum value is defined from the tag param
+			if params[0] != "" {
+				valid, minDuration := parseDurationWithDay(params[0])
+				if !valid {
 					return false
 				}
-				if duration > max {
-					// the duration value is larger than the max
+				if duration < minDuration {
+					// the duration value is smaller than the min
 					return false
+				}
+			}
+
+			if len(params) > 1 {
+				// Check if maximum value is defined from the tag param
+				if params[1] != "" {
+					valid, maxDuration := parseDurationWithDay(params[1])
+					if !valid {
+						return false
+					}
+					if duration > maxDuration {
+						// the duration value is larger than the max
+						return false
+					}
 				}
 			}
 		}
 	}
 	return true
+}
+
+// parseDurationWithDay extends duration string parsing to support the "d" (day) unit.
+// It returns a boolean indicating whether the string is valid, along with the corresponding Duration value.
+func parseDurationWithDay(durationStr string) (bool, time.Duration) {
+	// Duration string should not be empty
+	if durationStr == "" {
+		return false, time.Duration(0)
+	}
+
+	var totalDuration time.Duration
+
+	// Regex to find all day fragments like "2.2d", "1.5d", "1d1d", etc.
+	re := regexp.MustCompile(`([\d.]+)d`)
+	matches := re.FindAllStringSubmatch(durationStr, -1)
+
+	// Sum up all matched day durations
+	for _, match := range matches {
+		if len(match) != 2 {
+			continue
+		}
+		day, err := strconv.ParseFloat(match[1], 64)
+		if err != nil {
+			return false, 0
+		}
+		// Converts days to hours and adds up to the total duration
+		totalDuration += time.Duration(day * float64(24*time.Hour))
+	}
+
+	// Remove all day fragments from the original string to get the rest
+	// so we're left with only the remaining duration (e.g., "5h30m")
+	durationStr = re.ReplaceAllString(durationStr, "")
+	durationStr = strings.TrimSpace(durationStr)
+
+	// Parse the remaining standard duration if any
+	if durationStr != "" {
+		remainingDuration, err := time.ParseDuration(durationStr)
+		if err != nil {
+			return false, totalDuration
+		}
+
+		totalDuration += remainingDuration
+	}
+	return true, totalDuration
 }
 
 // ValidateDtoUuid used to check the UpdateDTO uuid pointer value
--- common/validator_test.go
+++ common/validator_test.go
@@ -7,7 +7,9 @@ package common
 
 import (
 	"testing"
+	"time"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/stretchr/testify/require"
 )
 
@@ -57,6 +59,144 @@ func TestValidate(t *testing.T) {
 			err := Validate(tt.input)
 			if tt.expected {
 				require.Error(t, err)
+			}
+		})
+	}
+}
+
+// TestValidateDuration tests the ValidateDuration function
+func TestValidateDuration(t *testing.T) {
+	validate := validator.New()
+	err := validate.RegisterValidation(dtoDurationTag, ValidateDuration)
+	require.NoError(t, err)
+
+	tests := []struct {
+		name        string
+		field       any
+		expectedErr bool
+	}{
+		{"valid - duration string without day", "10h30m", false},
+		{"valid - duration string with day", "1d", false},
+		{"valid - duration string with day and hour", "1d5h", false},
+		{"valid - duration string with fractional days and hour", "2.5d500ms", false},
+		{"valid - duration string with hour and day at last", "30m2d", false},
+		{"valid - duration string with min in ms and max in h", "30ms", false},
+		{"invalid - duration string with valid day and invalid minute", "1dxxm", true},
+		{"invalid - duration string with invalid day and valid second", "xxd30s", true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err = validate.Var(tt.field, dtoDurationTag)
+			if tt.expectedErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+// TestValidateDuration_WithMinMax tests the ValidateDuration function with min or max defined in the edgex-dto-duration annotation tag
+func TestValidateDuration_WithMinMax(t *testing.T) {
+	validate := validator.New()
+	err := validate.RegisterValidation(dtoDurationTag, ValidateDuration)
+	require.NoError(t, err)
+
+	tests := []struct {
+		name        string
+		field       any
+		min         string
+		max         string
+		expectedErr bool
+	}{
+		{"valid - duration string exceeds min with day", "12h30m", "0.5d", "", false},
+		{"valid - duration string exceeds min without day", "10h30m", "10h", "", false},
+		{"valid - duration string with day exceeds min with day", "1d", "0.5d", "", false},
+		{"valid - duration string with day and hour less than max with day", "1d5h", "", "2d", false},
+		{"valid - duration string with fractional days and hour less than max with day", "2.5d500ms", "", "3d", false},
+		{"invalid - duration string with day less than than min with day", "1d5h", "2d", "3d", true},
+		{"invalid - duration string with day exceeds the max with day", "4d6h", "2d", "3d", true},
+		{"invalid - duration string without day less than than min without day", "5ms10us", "6ms", "30ms", true},
+		{"invalid - duration string without day exceeds the max without day", "300ns", "100ns", "200ns", true},
+		{"invalid - duration string with invalid min", "1d", "xxd", "", true},
+		{"invalid - duration string with invalid max", "1d", "", "1dxxs", true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tagValue := dtoDurationTag + "="
+			if tt.min != "" {
+				tagValue += tt.min
+			}
+			if tt.max != "" {
+				tagValue += "0x2C" + tt.max
+			}
+			err = validate.Var(tt.field, tagValue)
+			if tt.expectedErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+// TestParseDurationWithDay tests the parseDurationWithDay function
+func TestParseDurationWithDay(t *testing.T) {
+	durStr1 := "10h30m"
+	expectedDur1, err := time.ParseDuration(durStr1)
+	require.NoError(t, err)
+
+	durStr2 := "1d"
+	expectedDur2, err := time.ParseDuration("24h")
+	require.NoError(t, err)
+
+	durStr3 := "1d5h"
+	expectedDur3, err := time.ParseDuration("29h")
+	require.NoError(t, err)
+
+	durStr4 := "2.5d500ms"
+	expectedDur4, err := time.ParseDuration("60h500ms")
+	require.NoError(t, err)
+
+	durStr5 := "30m2d"
+	expectedDur5, err := time.ParseDuration("30m48h")
+	require.NoError(t, err)
+
+	durStr6 := "30ms"
+	expectedDur6, err := time.ParseDuration(durStr6)
+	require.NoError(t, err)
+
+	durStr7 := "2.2d1.1d20m3h1h10m"
+	expectedDur7, err := time.ParseDuration("83.2h30m")
+	require.NoError(t, err)
+
+	tests := []struct {
+		name             string
+		durString        string
+		expectedResult   bool
+		expectedDuration time.Duration
+	}{
+		{"valid - duration string without day", durStr1, true, expectedDur1},
+		{"valid - duration string with day", durStr2, true, expectedDur2},
+		{"valid - duration string with day and hour", durStr3, true, expectedDur3},
+		{"valid - duration string with fractional days and hour", durStr4, true, expectedDur4},
+		{"valid - duration string with hour and day at last", durStr5, true, expectedDur5},
+		{"valid - duration string with min in ms and max in h", durStr6, true, expectedDur6},
+		{"valid - duration string with repeated day and other time units", durStr7, true, expectedDur7},
+		{"invalid - duration string with valid day and invalid minute", "1dxxm", false, 0},
+		{"invalid - duration string with invalid day and valid second", "xxd30s", false, 0},
+		{"invalid duration string", "abc", false, 0},
+		{"invalid - empty duration string", "", false, 0},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, duration := parseDurationWithDay(tt.durString)
+			require.Equal(t, tt.expectedResult, result)
+			if tt.expectedResult {
+				require.Equal(t, tt.expectedDuration, duration)
 			}
 		})
 	}
