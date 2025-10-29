22247c0ae3eec8a16a80e398639d8d667dd76d6f
Jude Hung <jude@iotechsys.com>
2025-10-29 13:52:46 +0800

    refactor!: update the Count field of CountResponse DTO from uint32 to int64
    Signed-off-by: Jude Hung <jude@iotechsys.com>

--- dtos/common/count.go
+++ dtos/common/count.go
@@ -3,11 +3,11 @@ package common
 // CountResponse defines the Response Content for GET count DTO.
 type CountResponse struct {
 	BaseResponse `json:",inline"`
-	Count        uint32 `json:"count"`
+	Count        int64 `json:"count"`
 }
 
 // NewCountResponse creates new CountResponse with all fields set appropriately
-func NewCountResponse(requestId string, message string, statusCode int, count uint32) CountResponse {
+func NewCountResponse(requestId string, message string, statusCode int, count int64) CountResponse {
 	return CountResponse{
 		BaseResponse: NewBaseResponse(requestId, message, statusCode),
 		Count:        count,
